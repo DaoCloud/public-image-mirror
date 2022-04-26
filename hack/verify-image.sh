2b015c2468ccc932b8e00f08ef796e11b6dc6bf7
Shiming Zhang <wzshiming@foxmail.com>
2022-04-26 18:24:35 +0800

    Checking the length of tags for verify-image

--- hack/verify-image.sh
+++ hack/verify-image.sh
@@ -10,7 +10,27 @@ git apply -R <(curl -fsSL "${patch_url}") || :
 
 list=$(diff --unified "${file}" "${file}.bak" | grep '^+\w' | sed 's/^+//' || :)
 
+failed=()
 for image in ${list}; do
     echo "Checking image: ${image}"
-    skopeo list-tags --retry-times 3 "docker://${image}" || { echo "Not Found ${image}" ; exit 1; }
+    raw=$(skopeo list-tags --no-creds --tls-verify=false --retry-times 3 "docker://${image}")
+    if [[ $? -ne 0 ]]; then
+        failed+=("not found ${image}")
+        echo "Not found ${image}"
+        continue
+    fi
+    if [[ $(echo "${raw}" | jq '.Tags | length') -eq 0 ]]; then
+        failed+=("found ${image} but no tags")
+        echo "Found ${image} but no tags"
+        echo "${raw}"
+        continue
+    fi
 done
+
+if [[ ${#failed[@]} -ne 0 ]]; then
+    echo "Failed images:"
+    for image in "${failed[@]}"; do
+        echo "  ${image}"
+    done
+    exit 1
+fi
