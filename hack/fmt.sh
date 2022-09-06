ff291afd7fc818482854892937651987fbadbfaa
Shiming Zhang <wzshiming@foxmail.com>
2022-09-06 12:46:04 +0800

    Add LC_ALL=C

--- hack/fmt.sh
+++ hack/fmt.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C
+
 cat $1 | sort -u | grep -v '^$' > $1.tmp && mv $1.tmp $1
--- hack/verify-fmt.sh
+++ hack/verify-fmt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C
+
 cp $1 $1.bak
 
 $(dirname "${BASH_SOURCE}")/fmt.sh $1.bak
