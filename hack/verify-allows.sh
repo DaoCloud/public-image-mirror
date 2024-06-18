5a3f3841d6322611082a96a4ff611145e446f7f8
Shiming Zhang <wzshiming@foxmail.com>
2024-06-18 16:51:10 +0800

    Add allows check for sync

---
+++ hack/verify-allows.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+file=$1
+
+image=$2
+
+function check_allows() {
+    local file=$1
+    local image=$2
+    while read line; do
+        if [[ "${line}" == *"**" ]]; then
+            if [[ "${image}" == "${line%\*\*}"* ]]; then
+                return 0
+            fi
+        elif [[ "${line}" == *"*" ]]; then
+            if [[ "${image}" == "${line%\*}"* ]]; then
+                if [[ "${image#"${line%\*}"}" != *"/"* ]]; then
+                    return 0
+                fi
+            fi
+        fi
+    done <"${file}"
+
+    return 1
+}
+
+check_allows "${file}" "${image}"
