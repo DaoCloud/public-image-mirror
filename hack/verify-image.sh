b6ece2809b379fffb5a5993a1a398d3e54714b57
Shiming Zhang <wzshiming@foxmail.com>
2024-07-17 19:11:07 +0800

    Fix verify image

--- hack/verify-image.sh
+++ hack/verify-image.sh
@@ -15,6 +15,7 @@ fi
 
 failed=()
 for image in ${list}; do
+    image="${image##:*}"
     echo "Checking image: ${image}"
     raw=$(skopeo list-tags --no-creds --tls-verify=false --retry-times 3 "docker://${image}")
     if [[ $? -ne 0 ]]; then
